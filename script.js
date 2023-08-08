3c93e0d0c8002a8e44634b4dc3ca1033a47d8490
cgmccarron <mccarron.christian13@gmail.com>
2023-08-08 17:36:03 -0500

    added nav menu and transitions to porjects

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 const toggleNav = () => {
   document.body.dataset.nav =
     document.body.dataset.nav === "true" ? "false" : "true";
-  console.log("button pushed" + document.body.dataset.nav);
+};
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+document.querySelector(".hacker-word").onmouseover = (event) => {
+  let iteration = 0;
+
+  clearInterval(interval);
+
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < iteration) {
+          return event.target.dataset.name[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+
+    if (iteration >= event.target.dataset.name.length) {
+      clearInterval(interval);
+    }
+
+    iteration += 1 / 3;
+  }, 30);
 };
