ec74e48ccc6697c6df5c8b34810916c795d78fee
cgmccarron <mccarron.christian13@gmail.com>
2023-08-25 12:21:01 -0500

    fixed hacker word bug

--- script.js
+++ script.js
@@ -3,15 +3,12 @@ const toggleNav = () => {
     document.body.dataset.nav === "true" ? "false" : "true";
 };
 
-console.log(document.querySelectorAll(".hacker-word"));
-
 const hackerwords = document.querySelectorAll(".hacker-word");
 
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-let interval = null;
-
 hackerwords.forEach((word) => {
+  let interval = null;
   word.onmouseover = (event) => {
     let iteration = 0;
 
@@ -37,28 +34,3 @@ hackerwords.forEach((word) => {
     }, 30);
   };
 });
-
-document.querySelector(".hacker-word").onmouseover = (event) => {
-  let iteration = 0;
-
-  clearInterval(interval);
-
-  interval = setInterval(() => {
-    event.target.innerText = event.target.innerText
-      .split("")
-      .map((letter, index) => {
-        if (index < iteration) {
-          return event.target.dataset.name[index];
-        }
-
-        return letters[Math.floor(Math.random() * 26)];
-      })
-      .join("");
-
-    if (iteration >= event.target.dataset.name.length) {
-      clearInterval(interval);
-    }
-
-    iteration += 1 / 3;
-  }, 30);
-};
